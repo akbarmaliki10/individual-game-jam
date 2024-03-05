9aa1b12b449ec9662e2c1bd8b843c55e8be5e54b
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2024-03-05 10:45:07 +0700

    add walking animation

--- scenes/characters/Player.gd
+++ scenes/characters/Player.gd
@@ -1,17 +1,40 @@
 extends CharacterBody2D
 
-@onready var anim = $AnimatedSprite2D
+@onready var animation = $AnimatedSprite2D
 
-const SPEED = 300.0
+@export var speed: int = 50
+
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func updateAnimation():
+	if velocity.length() == 0:
+		animation.stop()
+	
+	if velocity.x < 0:
+		#Left
+		animation.flip_h = false
+		animation.play("side_walk")
+	elif velocity.x > 0:
+		# Right
+		animation.flip_h = true
+		animation.play("side_walk")
+	elif velocity.y > 0:
+		animation.play("down_walk")
+	elif velocity.y < 0:
+		animation.play("up_walk")
+		
 func _ready():
-	anim.flip_h = true
-	anim.play("side_idle")
+	animation.flip_h = true
+	animation.play("side_idle")
 
 func handleInput():
-	var moveDirection = Input.get_vector("ui_right", "ui_left", "ui_up", "ui_down")
-	velocity = moveDirection
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection * speed
+
+func _physics_process(delta):
+	handleInput()
+	move_and_slide()
+	updateAnimation()
